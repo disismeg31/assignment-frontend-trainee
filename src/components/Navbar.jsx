630217ead4448c802e43d7afddea8b10091262ee
Megha KL <megha@nortvis.com>
2025-09-09 18:29:17 +0530

    navbar and list ui setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="flex p-4 h-10 bg-[#ffffff] justify-between items-center">
+        <p className="flex justify-center items-center p-4 !text-gray-300 text-sm">
+          {" "}
+          Home{" "}
+          <span>
+            <MdKeyboardArrowRight size={20} />
+          </span>{" "}
+          <span className="!text-[#31318e] font-semibold text-sm">Dashboard V2</span>
+        </p>
+      <div className="m-2">
+        <input 
+        placeholder="🔍︎ Search anything..."
+        className="bg-[#d6ddea] px-3 w-100 border border-[#afc3e6] rounded-md flex justify-center placeholder:text-gray-400" 
+        type="text" />
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/WidgetList.jsx
+++ src/components/WidgetList.jsx
@@ -2,7 +2,10 @@ import Widget from "./Widget"
 
 function WidgetList() {
   return (
-    <div>WidgetList</div>
+    <div className="p-6 flex justify-between items-center">
+      <p>CNAPP Dashboard</p>
+      <button className="bg-[#ffffff] p-2 rounded-md text-gray-500 border-2 border-gray-300 flex hover:cursor-pointer">Add Widget +</button>
+    </div>
   )
 }
 
